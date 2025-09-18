07424537aeedde6e87925f177ff0c591e7ab1a2c
CloudSalander <ruben.as532@gmail.com>
2025-09-18 11:12:19 +0200

    what i did in 30 minutes

--- class/Client.php
+++ class/Client.php
@@ -1,13 +1,30 @@
 <?php
 
 class Client {
+
+    const RESERVATION_NAME_MSG = "Name or reservation?";
+    const PEOPLE_AMOUNT_MSG = "How many people?";
+
     private string $name;
 
     public function __construct(string $name) {
         $this->name = $name;
     }
 
-    public function reserve(Pub $pub) {}
+    public function reserve(Pub $pub) {
+        $reserveName = $this->getReservationName();
+        $peopleNuber = $this->getPeopleNumber();
+    }
+
+    private function getReservationName(): string {
+        $name = readline(self::RESERVATION_NAME_MSG);
+        if(is_null($name) || $name == "") return $this->name;
+        return $name; 
+    }
+
+    private function getPeopleNumber(): int {
+        return intval(readline(self::PEOPLE_AMOUNT_MSG));
+    }
 
 
 }
