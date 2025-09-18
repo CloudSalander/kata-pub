5013eba933063bf1b15193e032ad641b660d5a36
CloudSalander <ruben.as532@gmail.com>
2025-09-18 11:05:32 +0200

    Now we can create Pubs

--- class/Pub.php
+++ class/Pub.php
@@ -1,7 +1,12 @@
 <?php
 
-
-
 class Pub {
 
+    private array $tables;
+    
+    public function __construct($tables) {
+        $this->tables = $tables;
+    }
+
+    public function checkReservation(){} 
 }
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php 
-
+//TODO: Maybe doing a constructor Pattern por generating Pubs?
 require('class/Client.php');
 require('class/Pub.php');
+//TODO: Maybe import Table insided Pub only?
 require('class/Table.php');
 
-$client = new Client("Pepe");
-$pub = new Pub();
-$table = new Table(1,3);
 
-var_dump($table);
+$table1 = new Table(1,3);
+$table2 = new Table(2,7);
+$table3 = new Table(3,10);
+$table4 = new Table(4,4);
+$tables = [$table1,$table2,$table3,$table4];
+
+$client = new Client("Pepe");
+$pub = new Pub($tables);
+var_dump($tables);
+
+
